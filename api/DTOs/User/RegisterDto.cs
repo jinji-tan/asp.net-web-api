3ec66a99c8d74fe9b8e631baa8044b1d000e0108
Louis Tan <louistan600@gmail.com>
2026-04-24 23:39:33 +0800

    backend

--- api/DTOs/User/RegisterDto.cs
+++ api/DTOs/User/RegisterDto.cs
@@ -7,9 +7,16 @@ namespace api.DTOs.User
         [Required, EmailAddress]
         public string Email { get; set; } = "";
 
-        [Required, StringLength(100, MinimumLength = 8)]
+        [Required]
+        [StringLength(100, MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\S{8,}$",
+    ErrorMessage = "Password must contain uppercase, lowercase, and a number.")]
         public string Password { get; set; } = "";
+        [Required]
+        [MaxLength(50)]
         public string FirstName { get; set; } = "";
+        [Required]
+        [MaxLength(50)]
         public string LastName { get; set; } = "";
     }
 }
