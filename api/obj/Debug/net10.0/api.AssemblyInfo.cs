8cf1f7d6f9fb3df9f6da285844cd1a9d349e7941
Louis Tan <louistan600@gmail.com>
2026-04-24 23:17:31 +0800

    Testing credential store

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd3a6becdd7fc14a8e94ddb03c883384bbdb6af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472f50e6ec5dc5ef13c5615aedb88c0c33eb1507")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
